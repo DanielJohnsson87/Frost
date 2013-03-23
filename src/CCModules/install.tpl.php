1f4afa2bef6b6af9315b3da60bd84c8a6c88cb0d
Daniel Johnsson <Danne_J87@hotmail.com>
2013-03-23 10:06:39 +0100

    Corrected the installpage.

--- src/CCModules/install.tpl.php
+++ src/CCModules/install.tpl.php
@@ -12,19 +12,3 @@
 <?php endforeach; ?>
 </tbody>
 </table>
-
-<h6>Password Hashing</h6>
-<p>What hashing algorithm do you want to use to encrypt your passwords? Choose from plain, md5salt, md5, sha1salt, sha1.</p>
-<?=$form['hashing_algorithm']->GetHTML()?>
-<h6>Language</h6>
-<p>What language are you using. English is default. Use html correct abbriviations. Engligh = en </p>
-<?=$form['language']->GetHTML()?>
-<h6>Url type</h6>
-<p> What url type do you wish to use? Method nr 1 is default.
-	<ul>
-		<li>0	=> index.php/controller/method/arg1/arg2/arg3</li>
-		<li>1 	=> controller/method/arg1/arg2/arg3</li>
-		<li>2 	=> index.php?q=controller/method/arg1/arg2/arg3</li>
-	</ul>
-<?=$form['url_type']->GetHTML()?>
-<?=$form['create']->GetHTML()?>
